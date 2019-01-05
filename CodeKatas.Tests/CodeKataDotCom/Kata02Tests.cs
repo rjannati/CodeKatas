b977f53c28b3f933f467726344f10e6a672a3855
Ruh Chauhan <sunay.chauhan@bedrosians.com>
2019-01-04 17:19:27 -0800

    initial commit

---
+++ CodeKatas.Tests/CodeKataDotCom/Kata02Tests.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Xunit;
+
+namespace CodeKatas.Tests.CodeKataDotCom
+{
+	public class Kata02Test_V1
+	{
+		protected Func<int, int[], int> Chop;
+
+		public Kata02Test_V1()
+		{
+			Chop = CodeKatas.CodeKataDotCom.KarateChop_V1.Chop;
+		}
+
+		[Theory]
+		[InlineData(3,new int[] { }, -1)]
+		[InlineData(3,new int[] {1}, -1)]
+		[InlineData(1, new int[] { 1 }, 0)]
+		[InlineData(1, new int[] { 1, 3, 5 }, 0)]
+		[InlineData(3, new int[] { 1, 3, 5 }, 1)]
+		[InlineData(5, new int[] { 1, 3, 5 }, 2)]
+		[InlineData(0, new int[] { 1, 3, 5 }, -1)]
+		[InlineData(2, new int[] { 1, 3, 5 }, -1)]
+		[InlineData(4, new int[] { 1, 3, 5 }, -1)]
+		[InlineData(6, new int[] { 1, 3, 5 }, -1)]
+		[InlineData(1, new int[] { 1, 3, 5, 7 }, 0)]
+		[InlineData(3, new int[] { 1, 3, 5, 7 }, 1)]
+		[InlineData(5, new int[] { 1, 3, 5, 7 }, 2)]
+		[InlineData(7, new int[] { 1, 3, 5, 7 }, 3)]
+		[InlineData(0, new int[] { 1, 3, 5, 7 }, -1)]
+		[InlineData(2, new int[] { 1, 3, 5, 7 }, -1)]
+		[InlineData(4, new int[] { 1, 3, 5, 7 }, -1)]
+		[InlineData(6, new int[] { 1, 3, 5, 7 }, -1)]
+		[InlineData(8, new int[] { 1, 3, 5, 7 }, -1)]
+		public void Test_Chop(int valueParam, int[] arrayParam, int expectedResult)
+		{
+			var result = Chop(valueParam, arrayParam);
+			Assert.Equal(expectedResult, result);
+		}
+	}
+
+	public class Kata02Test_V2: Kata02Test_V1
+	{
+		public Kata02Test_V2()
+		{
+			Chop = CodeKatas.CodeKataDotCom.KarateChop_V2.Chop;
+		}
+	}
+}
---
+++ CodeKatas/CodeKataDotCom/Kata02.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+/// <summary>
+/// Refer to http://codekata.com/kata/kata02-karate-chop/
+/// </summary>
+namespace CodeKatas.CodeKataDotCom
+{
+	public static class KarateChop_V1
+	{
+		public static int Chop(int value, int[] elementArray)
+		{
+			var result = -1;
+			var continueToSearch = true;
+			var startIndex = 0;
+			var length = elementArray.Length;
+			var endIndex = length - 1;
+			while(startIndex <= endIndex && continueToSearch)
+			{
+				var midpointIndex = (length / 2) + startIndex;
+				var valueToCompare = elementArray[midpointIndex];
+
+				if (valueToCompare == value)
+				{
+					result = midpointIndex;
+					continueToSearch = false;
+				}
+				else if (valueToCompare > value)
+				{
+					endIndex = midpointIndex - 1;
+					length = (endIndex - startIndex) + 1;
+					continueToSearch = true;
+				}
+				else //valueToCompare < value
+				{
+					startIndex = midpointIndex + 1;
+					length = (endIndex - startIndex) + 1; 
+					continueToSearch = true;
+				}
+			}
+			
+			return result;
+		}
+	}
+
+	public static class KarateChop_V2
+	{
+		public static int Chop(int value, int[] elementArray)
+		{
+			throw new NotImplementedException();
+		}
+	}
+}
