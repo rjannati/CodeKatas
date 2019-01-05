b91bbe1b8778a084015fc313008147a1e2c9f882
Ruh Chauhan <ruhchauhan@Ruhs-MacBook-Pro.local>
2019-01-04 21:44:10 -0800

    version with recursion

--- CodeKatas/CodeKataDotCom/Kata02.cs
+++ CodeKatas/CodeKataDotCom/Kata02.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Text;
+using System.Linq;
 
 /// <summary>
 /// Refer to http://codekata.com/kata/kata02-karate-chop/
@@ -48,7 +49,47 @@ namespace CodeKatas.CodeKataDotCom
 	{
 		public static int Chop(int value, int[] elementArray)
 		{
-			throw new NotImplementedException();
-		}
+            if (elementArray.Length == 0) { return -1; }
+
+            if(elementArray.Length == 1) 
+            {
+                var valueToCompare = elementArray[0];
+                return (valueToCompare == value) ? 0 : -1;
+            }
+            else
+            {
+                var midIndex = elementArray.Length / 2;
+                var valueToCompare = elementArray[midIndex];
+
+                if(valueToCompare == value)
+                {
+                    return midIndex;
+                }
+                else if (valueToCompare > value)
+                {
+                    var startIndex = 0;
+
+                    var slicedArray = elementArray
+                        .Where((w, i) => i >= startIndex && i < midIndex)
+                        .ToArray();
+
+                    return Chop(value, slicedArray);
+                }
+                else // valueToCompare < value
+                {
+                    var endIndex = elementArray.Length - 1;
+
+                    var slicedArray = elementArray
+                        .Where((w, i) => i > midIndex && i <= (elementArray.Length - 1)).ToArray();
+
+                    var middleResult = Chop(value, slicedArray);
+
+                    if(middleResult == -1) { return -1; }
+                    else { return middleResult + (midIndex + 1); }
+                }
+
+            }
+
+        }
 	}
 }
